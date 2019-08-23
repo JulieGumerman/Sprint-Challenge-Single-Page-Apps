917ce4e2b1be1184aa85f8ce5421811db0937095
bodhibrdr <julie.gumerman@gmail.com>
2019-08-23 13:24:39 -0600

    props fix

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,16 +2,13 @@ import React from "react";
 
 export default function CharacterCard(props) {
   return (
-    {props.characters.map(character => {
-      return (
-          <div className="card">
-              <h3>{character.name}</h3>
-              <img src={character.image} alt={character.name}/>
-              <p><span>Species: </span>{character.species}</p>
-              <p><span>Status: </span>{character.status}</p>
-              <p><span>Gender: </span>{character.gender}</p>
-          </div>
-      )
-  })}
+    <div className="card">
+        <h3>{props.character.name}</h3>
+        <img src={props.character.image} alt={props.character.name}/>
+        <p><span>Species: </span>{props.character.species}</p>
+        <p><span>Status: </span>{props.status}</p>
+        <p><span>Gender: </span>{props.gender}</p>
+    </div>
+
   )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function LocationsList() {}
+import LocationCard from "./LocationCard";
+
+export default function LocationsList() {
+    const [locations, setLocations] = useState([]);
+    useEffect(()=> {
+        axios.get("https://rickandmortyapi.com/api/location/")
+        .then(response => {
+            const locations=response.data.results;
+            setLocations(locations);
+            console.log("locations", locations);
+        })
+    }, []);
+
+    return (
+        <div className="card-container">
+            <div className="title"><h2>Locations</h2></div>
+            {locations.map(location=>{
+                return(
+                    // <div className="card" key={location.id}>
+                    //     <h3>{location.name}</h3>
+                    //     <p><span>Dimension: </span>{location.dimension}</p>
+                    //     <p><span>Type: </span>{location.type}</p>
+                    // </div>
+                    <LocationCard location={location} />
+                );
+            })}
+        </div>
+    )
+
+
+}
